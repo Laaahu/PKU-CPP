66ba94b3c1cff8d07e56a465ff42194ee6671e46
Laaahu <413956710@qq.com>
2024-04-28 06:27:48 +0800

    002:难一点的swap Accepted

---
+++ 002.cpp
@@ -0,0 +1,28 @@
+/*
+002:难一点的swap
+
+描述
+填空，使得程序输出结果是：
+5,3
+
+主要考察指针变量通过引用来交换
+*/
+
+#include <iostream>
+using namespace std;
+
+void swap(int* &a, int* &b)
+{
+	int * tmp = a;
+	a = b;
+	b = tmp;
+}
+int main()
+{
+	int a = 3,b = 5;
+	int * pa = & a;
+	int * pb = & b;
+	swap(pa,pb);
+	cout << *pa << "," << * pb;
+	return 0;
+}
